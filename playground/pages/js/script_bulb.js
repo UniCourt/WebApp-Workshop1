f4304a68f20771eb7e7d269dc4b2721eeb439e53
mis <noelneethanr@unicourt.com>
2022-07-26 11:40:28 +0530

    Changes

--- playground/pages/js/script_bulb.js
+++ playground/pages/js/script_bulb.js
@@ -1,23 +1,30 @@
-function changeImage() {
-    const image = document.getElementById('myImage');
-    const statusText = document.getElementById('bulb-status');
+const image = document.getElementById('myImage');
+const statusText = document.getElementById('bulb-status');
 
-    const waitSeconds = 2;
-    const miliSeconds = waitSeconds * 1000;
+const waitSeconds = 2;
+const miliSeconds = waitSeconds * 1000;
+
+function changeImage() {
+    console.log("Button clicked")
 
     if (image.src.match("bulbon")) {
-
+        
         setTimeout(function() {
             image.src = "assets/pic_bulboff.gif"; 
+            console.log("Turning off . . .")
         }, miliSeconds);
 
         statusText.innerHTML = "Off";
+        console.log("Turned off! complete")
+
     } else {
         
         setTimeout(function() {
             image.src = "assets/pic_bulbon.gif";
+            console.log("Turning on . . .")
         }, miliSeconds);
         
         statusText.innerHTML = "On";
+        console.log("Turned on! complete")
     }
 }
