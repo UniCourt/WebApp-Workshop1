1286909cef91d6bc1b676bb50180bf90414e097f
mis <noelneethanr@unicourt.com>
2022-07-26 11:15:51 +0530

    Bulb changes

--- playground/pages/js/script_bulb.js
+++ playground/pages/js/script_bulb.js
@@ -2,11 +2,22 @@ function changeImage() {
     const image = document.getElementById('myImage');
     const statusText = document.getElementById('bulb-status');
 
+    const waitSeconds = 2;
+    const miliSeconds = waitSeconds * 1000;
+
     if (image.src.match("bulbon")) {
-        image.src = "assets/pic_bulboff.gif";
+
+        setTimeout(function() {
+            image.src = "assets/pic_bulboff.gif"; 
+        }, miliSeconds);
+
         statusText.innerHTML = "Off";
     } else {
-        image.src = "assets/pic_bulbon.gif";
+        
+        setTimeout(function() {
+            image.src = "assets/pic_bulbon.gif";
+        }, miliSeconds);
+        
         statusText.innerHTML = "On";
     }
 }
