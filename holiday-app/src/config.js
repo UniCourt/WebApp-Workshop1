b04742ad8b8eca5e456770c0f4aceeb35ff46167
mis <noelneethanr@unicourt.com>
2022-08-10 12:29:12 +0530

    W1 - Initial chnages

--- holiday-app/src/config.js
+++
@@ -1,1 +0,0 @@
-const { Pool } = require("pg");
--- holiday-app/src/index.js
+++ holiday-app/src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const routes = require('./routes');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
