50a90fa472ce63d123d29f01ebc57c26796f729b
mis <noelneethanr@unicourt.com>
2022-08-16 14:41:47 +0530

    Rever here

--- holiday-app/src/index.ts
+++ holiday-app/src/index.ts
@@ -1,31 +1,64 @@
-import express, { Express, Request, Response } from 'express';
-import axios from 'axios';
+import express, { Express, Request, Response } from "express";
+import axios from "axios";
+import { Pool } from "pg";
+class Server {
+  
+  private app: express.Application;
+  private query: Pool;
 
-const app: Express = express();
-const port = 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h2>Express + TypeScript Server</h2>');
-});
-
-app.get('/country-list', async(req: Request, res: Response) => {
-  try {
-    const countryUrl : string = "https://date.nager.at/api/v3/AvailableCountries";
-    const countryList: ObjectDTO = await axios.get(countryUrl);
-    console.log(typeof(countryList))
-    res.send(countryList.data);
-
-  } catch (error: any) {
-    console.log(error.toString());
-    return { message: "Something Went Wrong!!" };
+  constructor() {
+    this.app = express();
+    this.portConfig();
+    this.routes();
   }
-});
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  //Port configuration 
+  public portConfig() {
+    this.app.set("port", 3000);
+  }
+
+  //DB configuration
+  public dbCOnfig() {
+    this.query = new Pool({
+      host: process.env["POSTGRES_HOST"],
+      port: parseInt(<string>process.env["POSTGRES_PORT"]),
+      user: process.env["POSTGRES_USER"],
+      password: process.env["POSTGRES_PASSWORD"],
+      database: process.env["POSTGRES_DB"]
+
+    });
+  }
+
+  public routes() {
+
+    this.app.get('/', (req: Request, res: Response)=> {
+      res.send('<h2>Express + TypeScript Server</h2>');
+    });
+
+    this.app.get("/country-list", async (req: Request, res: Response) => {
+      try {
+        const countryUrl: string =
+          "https://date.nager.at/api/v3/AvailableCountries";
+        const countryList: ObjectDTO = await axios.get(countryUrl);
+        console.log(`Type of response: ${typeof countryList}`);
+        res.send(countryList.data);
+      } catch (error: any) {
+        console.log(error.toString());
+        return { message: "Something Went Wrong!!" };
+      }
+    });
+  }
+
+  public start() {
+    this.app.listen(this.app.get('port'), function() {
+      console.log(`⚡️[server]: Server is running at http://localhost:3000`);
+    })
+  }
+}
+const server = new Server();
+server.start();
 
 
 interface ObjectDTO {
-  data: object[]
+  data: object[];
 }
